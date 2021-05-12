62e3a27daf6ea787ea5716f59aea19a9c0e56d07
Hugo Calmels <calmels.hugo@yahoo.fr>
2021-05-12 15:19:08 +0200

    1.00

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  
+  get '/contact', to: 'static_pages#contact'
+  get '/team', to: 'static_pages#team'
+  get '/index', to: "static_pages#index"
+  get 'message/:user_entry', to: 'message#show'
+
+  resources :gossips, only: [:new, :create, :index]
+  
+end
